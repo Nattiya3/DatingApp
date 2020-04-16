04cdab941ba05a3e61490aabc2beb192aa791ab4
Nattiya Selvakumar <Nattiya.Selvakumar@in.ey.com>
2020-04-16 16:42:31 +0530

    Initial  Commit

---
+++ DatingApp.API/vscode/Data/DataContext.cs
@@ -0,0 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using DatingApp.API.vscode.Models;
+namespace DatingApp.API.vscode.Data
+
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options){}
+
+        public DbSet<Value> Values {get; set;}
+        
+    }
+}
